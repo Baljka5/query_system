6ecee5896bc83540566d26cdfb11869953cb88bd
baljka baljka <baljinnyambaljka5@gmail.com>
2025-10-29 13:42:20 +0800

    final

--- queryvault/settings.py
+++ queryvault/settings.py
@@ -4,13 +4,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dev-secret-key-please-change")
-DEBUG = os.getenv("DEBUG", "1") == "1"
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
     "127.0.0.1,localhost,it-mgt.cumongol.mn"
 ).split(",")
 
-DEBUG = False
+DEBUG = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
